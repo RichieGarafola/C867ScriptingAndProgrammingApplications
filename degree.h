0226ef3e6ff36714c67d5e92a4145df544ca04d1
Richie Garafola <99199925+RichieGarafola@users.noreply.github.com>
2023-02-24 10:44:09 -0500

    Update degree.h

---
+++ degree.h
@@ -0,0 +1,6 @@
+// Ensure that the contents of the file are only included once in the compilation process.
+#pragma once
+
+
+// define an enumerated data type called DegreeProgram that has three possible values: SECURITY, NETWORK, and SOFTWARE. 
+enum class DegreeProgram { SECURITY, NETWORK, SOFTWARE };
